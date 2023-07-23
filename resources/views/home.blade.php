794a2a81b66c5997fc6a040b94777e038405cdd5
chakii19 <iyanryan939@gmail.com>
2023-07-23 07:54:24 -0700

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,24 @@
 @extends('main')
 
 @section('title','Dasboard')
-    
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header float-left">
+            <div class="page-title">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-8">
+        <div class="page-header float-right">
+            <div class="page-title">
+                <ol class="breadcrumb text-right">
+                    <li class="active"><i class="fa fa-dashboard"></i></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
 @section('breadcrumbs')
     
 @endsection
